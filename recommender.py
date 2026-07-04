c31add7604e773fe9669be1b5446d9d9424011c1
Jaskiran <jaskiransaini06@gmail.com>
2026-07-04 17:11:23 +0530

    Initial commit-Loaded datasets

---
+++ recommender.py
@@ -0,0 +1,9 @@
+import pandas as pd
+#LOADING DATASET
+basics=pd.read_csv('Data/title.basics.tsv.gz',sep='\t',na_values='\\N',low_memory=False)
+ratings=pd.read_csv('Data/title.ratings.tsv.gz',sep='\t',na_values='\\N')
+crew=pd.read_csv('Data/title.crew.tsv.gz',sep='\t',na_values='\\N')
+names=pd.read_csv('Data/name.basics.tsv.gz',sep='\t',na_values='\\N')
+"""print(basics.shape,ratings.shape,crew.shape,names.shape)
+print(basics.head())"""
+
