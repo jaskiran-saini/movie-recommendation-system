073be3da2e47fe84b32b685172fd10f1f29b538e
Jaskiran <jaskiransaini06@gmail.com>
2026-07-04 17:18:05 +0530

    Filtered data

--- recommender.py
+++ recommender.py
@@ -6,4 +6,7 @@ crew=pd.read_csv('Data/title.crew.tsv.gz',sep='\t',na_values='\\N')
 names=pd.read_csv('Data/name.basics.tsv.gz',sep='\t',na_values='\\N')
 """print(basics.shape,ratings.shape,crew.shape,names.shape)
 print(basics.head())"""
+#FILTERING DATA
+movies=basics[basics['titleType']=='movie'].copy()
+print(f"Movies only: {movies.shape}")
 
